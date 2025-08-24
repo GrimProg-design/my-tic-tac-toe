a64aa917bdf5f2669246874f79866b706524ead8
GrimProg <vavilovila01@gmail.com>
2025-08-24 20:06:24 +0600

    Удалили бота с главной ветки

--- script.js
+++ script.js
@@ -7,17 +7,6 @@ function disabledButtons() {
   }
 }
 
-function bot() {
-  const field = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8]
-  ]
-
-  const brain = Math.floor(Math.random() * 9);
-  return brain;
-}
-
 function winOrLose(moves, winer) {
   const win = [
     [0, 1, 2],
